f2e1c32e39c07de6ffa8de552f19434e02fa87e2
paprikaLang <3060866223@qq.com>
2019-01-26 06:01:09 +0800

    route controller blade

---
+++ config/blog.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paprika
+ * Date: 2019/1/26
+ * Time: 5:52 AM
+ * config('blog.title') 将会返回 title 配置项的值
+ */
+return [
+    'title' => 'My Blog',
+    'posts_per_page' => 5
+];
